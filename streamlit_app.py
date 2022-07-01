86438613d1798459caf82f735f6d274a05481962
SFCOURSE2022 <108536599+SFCOURSE2022@users.noreply.github.com>
2022-07-01 16:24:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@
 import streamlit
 
 streamlit.title('My Parents New healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+Streamlit.text('Omega 3 and Blueberry Oatmeal')
+
+Streamlit.text('Kale, Spinach and Rocket Smoothie')
+
+Streamlit.text('Hard-Boiled Free-Range Egg')
+
