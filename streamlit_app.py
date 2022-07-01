e217e2b79c3390dfe95dd8790a603181e7fe77d7
SFCOURSE2022 <108536599+SFCOURSE2022@users.noreply.github.com>
2022-07-01 16:26:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ streamlit.title('My Parents New healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-Streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
 
-Streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
 
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
