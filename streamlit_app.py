c6ccadc90dd0f6558beaae4a3915c777eb23c47e
SFCOURSE2022 <108536599+SFCOURSE2022@users.noreply.github.com>
2022-07-08 16:52:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,4 +78,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("Fruit list:")
 streamlit.dataframe(my_data_rows)
 
+fruit_to_add = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('The user entered ', fruit_to_add)
+
+
 
